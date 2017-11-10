fbefcf2dd2dcbf0d33ae2515f48141940be4a2ce
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-10 04:52:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const driver = {} ;
+driver.name = "Sam";
+
+function updateDriverWithKeyAndValue() {
+  const newObj = {...driver};
+  newObj.address = "11 Broadway";
+  return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  driver.address = "12 Broadway";
+  return driver;
+}
+
+function deleteFromDriverByKey() {
+  const newdriver = {...driver};
+  delete newdriver.name;
+  return newdriver;
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  delete driver.name;
+  return Object.keys({}, driver, { name : '' });
+}
